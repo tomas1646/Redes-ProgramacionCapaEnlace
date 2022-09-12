796fe51c1c68e712ee63ac6467b56e4407ec240c
JokinAbarzua <jokinabarzua22@hotmail.com>
2022-09-12 10:41:19 -0300

    agrego el .sh para ejecutarlo con xfce

---
+++ CorrerProgramaXfce4.sh
@@ -0,0 +1,17 @@
+## Corres este script desde la terminal con el comando:
+## "bash CorrerPrograma.sh"
+javac src/BitUtils.java
+javac -cp src src/Package.java
+xfce4-terminal --tab -e 'bash -c "
+  bash $(pwd)/AbrirPuertos.sh;
+  bash"'
+
+xfce4-terminal --tab -e 'bash -c "
+  sleep 1;
+  bash $(pwd)/CompilarCorrerEmisor.sh;
+  bash"'
+
+xfce4-terminal --tab -e 'bash -c "
+  sleep 1
+  bash $(pwd)/CompilarCorrerReceptor.sh;
+  bash"'
